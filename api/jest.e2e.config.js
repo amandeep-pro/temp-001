2fec49084a36a49a9cdc4b6598759c67fa1046ac
Amandeep <->
2025-07-09 04:14:46 -0400

    chore: re-init

--- api/jest.e2e.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['<rootDir>/tests/e2e/**/*.test.js'],
-  testTimeout: 30000,
-  setupFilesAfterEnv: ['<rootDir>/tests/e2e/setup.js'],
-  globalTeardown: '<rootDir>/tests/e2e/teardown.js'
-};
